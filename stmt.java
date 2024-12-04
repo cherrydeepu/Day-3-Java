ebb3615c7fcedcfdd98f20afd33bbac39155dc98
cherrydeepu <trishithapandu2218@gmail.com>
2024-12-04 14:19:08 +0530

    Add files via upload

---
+++ stmt.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class stmt{
+    public static void main(String args[]){
+        Scanner S=new Scanner(System.in);
+        int a=S.nextInt();
+        if(a==2){
+            System.out.println("True");
+        }
+    }
+}
