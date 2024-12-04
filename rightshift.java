216ff6327fde7f7ac14a779e040672c2766dd6d3
cherrydeepu <trishithapandu2218@gmail.com>
2024-12-04 13:38:44 +0530

    Add files via upload

---
+++ rightshift.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class rightshift{
+    public static void main(String args[]){
+        Scanner S=new Scanner(System.in);
+        int a=S.nextInt();
+        int b=S.nextInt();
+        System.out.println(a>>b);
+    }
+}
